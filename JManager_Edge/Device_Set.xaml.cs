c52abf13312a503ed05df5912167252b30468c15
준범 구 <9junbeum@naver.com>
2023-02-20 10:52:32 +0900

    수정

--- JManager_Edge/Device_Set.xaml.cs
+++ JManager_Edge/Device_Set.xaml.cs
@@ -21,29 +21,37 @@ namespace JManager_Edge
     {
         Device_Data deviceData = Device_Data.instance;
         private int arr_num;
+        bool loaded = false;
 
         public Device_Set(int i)
         {
             InitializeComponent();
             this.arr_num = i;
+            loaded = true;
         }
 
         private void restart(object sender, RoutedEventArgs e)
         {
-            //장치 재시작
-            string url = ""; 
+        //장치 재시작
+            string url = "http://" + deviceData.Devices[arr_num].IP + "/axis-cgi/restart.cgi"; 
             deviceData.Send_Url(url, arr_num);
-            system_.Text += "Gain 값" + gain_slider.Value.ToString() + "로 설정되었습니다.\n";
+            system_.Text += "장치가 재부팅 됩니다.\n";
         }
 
         private void factory_reset(object sender, RoutedEventArgs e)
         {
             //공장 초기화
+            //string url = "http://" + deviceData.Devices[arr_num].IP + "/axis-cgi/hardfactorydefault.cgi";
+            //deviceData.Send_Url(url, arr_num);
+            //system_.Text += "장치 공장초기화를 시작합니다. \n";
+
         }
 
         private void sound_correction(object sender, RoutedEventArgs e)
         {
             //보정 테스트
+
+
         }
 
         private void close(object sender, RoutedEventArgs e)
@@ -55,12 +63,14 @@ namespace JManager_Edge
 
         private void gain_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            string url = "http://" + deviceData.Devices[arr_num].IP + "/axis-cgi/param.cgi?action=update&AudioSource.A0.OutputGain=" + gain_slider.Value;
-            deviceData.Send_Url(url, arr_num);
-
-
-
-            system_.Text += "Gain 값" + gain_slider.Value.ToString() + "로 설정되었습니다.\n";
+            if(loaded)
+            {
+                string url = "http://" + deviceData.Devices[arr_num].IP + "/axis-cgi/param.cgi?action=update&AudioSource.A0.OutputGain=" + gain_slider.Value;
+                string result = deviceData.Send_Url(url, arr_num);
+                system_.Text += "Gain 값" + gain_slider.Value.ToString() + "로 설정되었습니다.\n";
+                system_.Text += result;
+            }
         }
+
     }
 }
